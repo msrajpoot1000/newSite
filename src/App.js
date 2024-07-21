7211e77a2ced9295519bff117e4188a4350e0374
manish <msrajpoot1000@gmail.com>
2024-07-21 16:04:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "@fontsource/heebo/400.css";
+import "@fontsource/heebo/500.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+//bootstrap loding
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//edited boostrap import
+import "./assets/bootsrapCss/bootstrap.min.css";
+
+// import all in one css
+import "./assets/css/style.css";
+
+// In src/index.js or src/App.js
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+// import pagees
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+
+//import router setting
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
